dd01ba583b8cf503065992fe34043ee24b872e36
Greg Vondra <gvondra@gmail.com>
2022-11-29 12:35:40 -0600

    add user name caching

--- Source/Server/Interface.Authorization/UserService.cs
+++ Source/Server/Interface.Authorization/UserService.cs
@@ -1,5 +1,9 @@
 ﻿using YardLight.Interface.Authorization.Models;
 using BrassLoon.RestClient;
+using Microsoft.Extensions.Caching.Memory;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +15,7 @@ namespace YardLight.Interface.Authorization
 {
     public class UserService : IUserService
     {
+        private static readonly Policy _userNameCache = Policy.Cache(new MemoryCacheProvider(new MemoryCache(new MemoryCacheOptions())), new SlidingTtl(TimeSpan.FromSeconds(90)));
         private readonly RestUtil _restUtil;
         private readonly IService _service;
 
@@ -44,6 +49,13 @@ namespace YardLight.Interface.Authorization
         }
 
         public async Task<string> GetName(ISettings settings, Guid id)
+        {
+            return await _userNameCache.Execute(async (context) => await InnerGetName(settings, id),
+                new Context(id.ToString("N"))
+                );
+        }
+
+        private async Task<string> InnerGetName(ISettings settings, Guid id)
         {
             UriBuilder builder = new UriBuilder(settings.BaseAddress);
             builder.Path = _restUtil.AppendPath(builder.Path, "User", id.ToString("N"), "Name");
