5921f40142d4025a4d08cfd650418661ab072411
Greg Vondra <gvondra@gmail.com>
2022-12-02 11:07:45 -0600

    update save work item

--- Source/Client/Client/Backlog/ViewModels/WorkItemVM.cs
+++ Source/Client/Client/Backlog/ViewModels/WorkItemVM.cs
@@ -37,6 +37,20 @@ namespace YardLight.Client.Backlog.ViewModels
         public ObservableCollection<WorkItemVM> Children => _children;
         public string ColorCode => _innerWorkItem.Type.ColorCode;
         public string StatusTitle => _innerWorkItem.Status?.Title;
+        
+        public WorkItemStatus Status
+        {
+            get => _innerWorkItem.Status;
+            set
+            {
+                if (_innerWorkItem.Status != value)
+                {
+                    _innerWorkItem.Status = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged(nameof(StatusTitle));
+                }
+            }
+        }
 
         public string Title
         { 
@@ -50,7 +64,111 @@ namespace YardLight.Client.Backlog.ViewModels
                 }
             }
         }
-        
+
+        public string Team
+        {
+            get => _innerWorkItem.Team;
+            set
+            {
+                if (_innerWorkItem.Team != value)
+                {
+                    _innerWorkItem.Team = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Itteration
+        {
+            get => _innerWorkItem.Itteration;
+            set
+            {
+                if (_innerWorkItem.Itteration != value)
+                {
+                    _innerWorkItem.Itteration = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Effort
+        {
+            get => _innerWorkItem.Effort;
+            set
+            {
+                if (_innerWorkItem.Effort != value)
+                {
+                    _innerWorkItem.Effort = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Priority
+        {
+            get => _innerWorkItem.Priority;
+            set
+            {
+                if (_innerWorkItem.Priority != value)
+                {
+                    _innerWorkItem.Priority = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Value
+        {
+            get => _innerWorkItem.Value;
+            set
+            {
+                if (_innerWorkItem.Value != value)
+                {
+                    _innerWorkItem.Value = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime? TargetDate
+        {
+            get => _innerWorkItem.TargetDate;
+            set
+            {
+                if (_innerWorkItem.TargetDate != value)
+                {
+                    _innerWorkItem.TargetDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime? StartDate
+        {
+            get => _innerWorkItem.StartDate;
+            set
+            {
+                if (_innerWorkItem.StartDate != value)
+                {
+                    _innerWorkItem.StartDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime? CloseDate
+        {
+            get => _innerWorkItem.CloseDate;
+            set
+            {
+                if (_innerWorkItem.CloseDate != value)
+                {
+                    _innerWorkItem.CloseDate = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
         public Brush BulletColor
         {
             get => _bulletColor;
@@ -63,6 +181,19 @@ namespace YardLight.Client.Backlog.ViewModels
                 }
             }
         }
+
+        public List<WorkItemStatus> AvailableStatuses
+        {
+            get
+            {
+                return _innerWorkItem.Type.Statuses
+                    .Where(s => s.WorkItemStatusId.Equals(_innerWorkItem.Status.WorkItemStatusId.Value) || (s.IsActive ?? false))                    
+                    .OrderBy(s => s.Order)
+                    .Select(s => s.WorkItemStatusId.Value.Equals(Status.WorkItemStatusId.Value) ? Status : s)
+                    .ToList();
+            }
+        }
+
         public void AddBehavior(object behavior) => _behaviors.Add(behavior);
     }
 }
