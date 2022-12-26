4c4704bf8a39df28ccf2d51b6df68cda3edec774
Greg Vondra <gvondra@gmail.com>
2022-12-26 09:14:28 -0600

    add empty board page; add link to board page

--- Source/Client/Client/MainWindow.xaml.cs
+++ Source/Client/Client/MainWindow.xaml.cs
@@ -1,21 +1,8 @@
-﻿using Autofac;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using YardLight.Client.ViewModel;
-using YardLight.Interface;
-using Models = YardLight.Interface.Models;
 
 namespace YardLight.Client
 {
@@ -102,42 +89,6 @@ namespace YardLight.Client
             UserSession userSession = UserSessionLoader.GetUserSession();
             MainWindowVM.ShowProjectSettings = BoolToVisibility(userSession?.OpenProjectId != null);
             MainWindowVM.ShowProject = BoolToVisibility(userSession?.OpenProjectId != null);
-            //Task.Run(() => GetProject(userSession?.OpenProjectId))
-            //    .ContinueWith(GetProjectCallback, userSession?.OpenProjectId, TaskScheduler.FromCurrentSynchronizationContext());
         }
-
-        //private Task<Models.Project> GetProject(Guid? projectId)
-        //{
-        //    if (projectId.HasValue)
-        //    {
-        //        using (ILifetimeScope scope = DependencyInjection.ContainerFactory.Container.BeginLifetimeScope())
-        //        {
-        //            ISettingsFactory settingsFactory = scope.Resolve<ISettingsFactory>();
-        //            IProjectService projectService = scope.Resolve<IProjectService>();
-        //            return projectService.Get(settingsFactory.CreateApi(), projectId.Value);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        return Task.FromResult(default(Models.Project));
-        //    }
-        //}
-
-        //private async Task GetProjectCallback(Task<Models.Project> getProject, object state)
-        //{
-        //    try
-        //    {
-        //        if (state != null)
-        //        {
-        //            Models.Project project = await getProject;
-        //            if (project == null || project.ProjectId == (Guid)state)
-        //                MainWindowVM.Project = project;
-        //        }
-        //    }
-        //    catch (System.Exception ex)
-        //    {
-        //        ErrorWindow.Open(ex, this);
-        //    }
-        //}
     }
 }
